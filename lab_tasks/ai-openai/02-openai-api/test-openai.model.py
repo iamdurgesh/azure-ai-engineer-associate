1c8bec3f428ca29a1d1d5b7cffa0d4f0361224c4
iamdurgesh <59917822+iamdurgesh@users.noreply.github.com>
2024-10-27 23:01:15 +0000

    # chatCompletion in openapi

--- lab_tasks/ai-openai/02-openai-api/test-openai.model.py
+++ lab_tasks/ai-openai/02-openai-api/test-openai.model.py
@@ -41,19 +41,35 @@ def main():
             
             # Add code to send request...
             # Send request to Azure OpenAI model
+            # response = client.chat.completions.create(
+            #     model=azure_oai_deployment,
+            #     temperature=0.7,
+            #     max_tokens=400,
+            #     messages=[
+            #         {"role": "system", "content": system_message},
+            #         {"role": "user", "content": input_text}
+            #     ]
+            # )
+            # generated_text = response.choices[0].message.content
+
+            # # Print the response
+            # print("Response: " + generated_text + "\n")
+            
+            # Send request to Azure OpenAI model
+            messages_array.append({"role": "user", "content": input_text})
+
             response = client.chat.completions.create(
                 model=azure_oai_deployment,
                 temperature=0.7,
-                max_tokens=400,
-                messages=[
-                    {"role": "system", "content": system_message},
-                    {"role": "user", "content": input_text}
-                ]
+                max_tokens=1200,
+                messages=messages_array
             )
             generated_text = response.choices[0].message.content
+            # Add generated text to messages array
+            messages_array.append({"role": "assistant", "content": generated_text})
 
-            # Print the response
-            print("Response: " + generated_text + "\n")
+            # Print generated text
+            print("Summary: " + generated_text + "\n")
             
             
 
